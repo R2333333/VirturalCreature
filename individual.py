cf965888655cdd2facc624a8aa5ae10158e02c47
Roy Xu <roy.xu@carleton.ca>
2020-12-17 01:11:30 +0000

    hill climber

--- individual.py
+++ individual.py
@@ -1,19 +1,24 @@
 import pyrosim
 from robot import ROBOT
-from random import random
+import random as rd
+import math
 
 
 class INDIVIDUAL:
 
     def __init__(self):
-        self.genome = random() * 2 - 1
+        self.genome = rd.random() * 2 - 1
         self.fitness = 0
 
-    def evaluate(self):
-        sim = pyrosim.Simulator(eval_time=1000)
+    def evaluate(self, eval_time=1000, play_blind = False):
+        sim = pyrosim.Simulator(eval_time=eval_time, play_blind=play_blind)
         robot = ROBOT(sim, self.genome)
 
         sim.start()
         sim.wait_to_finish()
 
         self.fitness = sim.get_sensor_data( sensor_id = robot.P4 , svi = 1 )[-1]
+        return self.fitness
+
+    def mutate(self):
+        self.genome = rd.gauss( self.genome , math.fabs(self.genome) )
